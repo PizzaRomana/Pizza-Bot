c898a18a2a3b3406377eaef6a4f1d01109be193d
PizzaRomana <79379098+PizzaRomana@users.noreply.github.com>
2021-02-20 17:29:51 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,79 @@
+const help = (prefix, pushname, botname, oname) => {
+	return `
+  「 *Meu Bot* 」
+࿇ ══━━━━✥◈✥━━━━══ ࿇
+  SALVE✌️🏻
+  
+  ◪ *INFORMAÇÕES*
+    ❏ Prefix: 「  ${prefix}  」
+    ❏ Criador: PizzaRomana
+    ❏ Numero do meu criador:(11)94643-5579
+ ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    Outros Menus
+    ❏${prefix}nsfwmenu
+    ❏${prefix}menuadmin
+  
+    
+  ◪ *LEVEL*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}leveling
+    └─ ❏ ${prefix}level
+  ◪ *CRIAR*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}sticker
+    ├─ ❏ ${prefix}toimg
+    ├─ ❏ ${prefix}bpink
+    ├─ ❏ ${prefix}marvellogo
+    ├─ ❏ ${prefix}snowwrite
+    ├─ ❏ ${prefix}3dtext
+    ├─ ❏ ${prefix}ninjalogo
+    ├─ ❏ ${prefix}water
+    ├─ ❏ ${prefix}firetext
+    ├─ ❏ ${prefix}neonlogo
+    ├─ ❏ ${prefix}lionlogo
+    ├─ ❏ ${prefix}jokerlogo
+    └─ ❏ ${prefix}quotemaker
+  ◪ *PESQUISA*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}wikien (Apenas pesquisas em Ingles)
+  ◪ *DOWNLOADER*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}images 
+    ├─ ❏ ${prefix}ytmp3
+    ├─ ❏ ${prefix}ytmp4
+    ├─ ❏ ${prefix}joox
+    ├─ ❏ ${prefix}joox2
+    ├─ ❏ ${prefix}trendtwit
+    └─ ❏ ${prefix}ytsearch
+  ◪ *MEME*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}meme
+  ◪ *Roleta Harém*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}neonime
+    ├─ ❏ ${prefix}pokemon
+    ├─ ❏ ${prefix}loli
+    ├─ ❏ ${prefix}waifu
+    ├─ ❏ ${prefix}walpaper
+    ├─ ❏ ${prefix}neko
+    └─ ❏ ${prefix}nekonime
+  ◪ *OUTROS*
+  ࿇ ══━━━━✥◈✥━━━━══ ࿇
+    │
+    ├─ ❏ ${prefix}send
+    ├─ ❏ ${prefix}wame
+    ├─ ❏ ${prefix}virtex
+    ├─ ❏ ${prefix}exe
+    ├─ ❏ ${prefix}qrcode
+    ├─ ❏ ${prefix}quotes
+    └─ ❏ ${prefix}fml
+  `
+}
+
+exports.help = help
---
+++ src/nsfwmenu.js
@@ -0,0 +1,20 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `Bom, esse é o meno NSFW, se você não sabe oq é, é recomendado não usar
+    ◪ *NSFW*
+  │
+  ├─ ❏ ${prefix}nsfwbobs
+  ├─ ❏ ${prefix}randomhentaio
+  ├─ ❏ ${prefix}nsfwass (OFF)
+  ├─ ❏ ${prefix}nsfwsidebobs
+  ├─ ❏ ${prefix}nsfwahegao
+  ├─ ❏ ${prefix}nsfwthighs
+  ├─ ❏ ${prefix}nsfwarmpits
+  └─ ❏ ${prefix}nsfwfeets
+
+  _obs para usar esses comandos ative o menu NSFW_\n _Digite_\n ${prefix}*nsfw 1*`
+
+
+
+}
+
+exports.nsfwmenu = nsfwmenu
