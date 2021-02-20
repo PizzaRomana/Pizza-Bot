51669525da900da55f4c7d002334cd261f297bec
PizzaRomana <79379098+PizzaRomana@users.noreply.github.com>
2021-02-20 18:27:19 -0300

    Update nsfwmenu.js

--- src/nsfwmenu.js
+++ src/nsfwmenu.js
@@ -1,14 +1,16 @@
 const nsfwmenu = (prefix, pushname) => {
-    return `Bom, esse é o meno NSFW, se você não sabe oq é, é recomendado não usar
+    return `Bom, esse é o menu NSFW, se você não sabe oq é, é recomendado não usar
     ◪ *NSFW*
   │
   ├─ ❏ ${prefix}nsfwbobs
   ├─ ❏ ${prefix}randomhentaio
-  ├─ ❏ ${prefix}nsfwass (OFF)
+  ├─ ❏ ${prefix}nsfwass
   ├─ ❏ ${prefix}nsfwsidebobs
   ├─ ❏ ${prefix}nsfwahegao
   ├─ ❏ ${prefix}nsfwthighs
   ├─ ❏ ${prefix}nsfwarmpits
+  ├─ ❏ ${prefix}toin
+  ├─ ❏ ${prefix}nsfwblowjob
   └─ ❏ ${prefix}nsfwfeets
 
   _obs para usar esses comandos ative o menu NSFW_\n _Digite_\n ${prefix}*nsfw 1*`
